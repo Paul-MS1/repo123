e72d8bf8270470f65a99f9f6e7e3cd64edadda69
PaulMetry <132548977+PaulMetry@users.noreply.github.com>
2023-05-04 11:41:24 +0200

    Add files via upload

---
+++ IFElse.py
@@ -0,0 +1,14 @@
+a = int(input("Zahl 1: "))
+
+#if [Bedingung]:
+##   <->BLOCK1 (1TAB rechtsversetzt)
+#else
+##   <->Block 2
+
+if a == 0:
+    print("Zahl 1 = 0!")
+    input("Zahl die nicht 0 ist bitte")
+    print("Zahl", a)
+
+else:
+    print ("Alles Gut")
