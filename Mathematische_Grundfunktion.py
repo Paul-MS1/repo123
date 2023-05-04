6a16f564c787486e8653714323ffe08b082dddd7
PaulMetry <132548977+PaulMetry@users.noreply.github.com>
2023-05-04 10:29:17 +0200

    Add files via upload

---
+++ Mathematische_Grundfunktion.py
@@ -0,0 +1,13 @@
+a = float(input("Geben Sie die erste Zahl ein: "))
+b = float(input("Geben Sie die zweite Zahl ein: "))
+
+summe = a + b
+differenz = a - b
+produkt = a * b
+quotient = a / b
+
+print("Summe:", summe)
+#print("Summe", a+b)
+print("Differenz:", differenz)
+print("Produkt:", produkt)
+print("Quotient:", quotient)
